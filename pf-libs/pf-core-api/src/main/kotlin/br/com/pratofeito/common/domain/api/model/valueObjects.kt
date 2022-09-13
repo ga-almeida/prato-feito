3661c4e468de290fef3575cd797ec9023835f6df
Gabriel Almeida <gabrielsantos.almeida@hotmail.com>
2022-09-13 00:54:44 -0300

    core-api-domain

--- pf-libs/pf-core-api/src/main/kotlin/br/com/pratofeito/common/domain/api/model/valueObjects.kt
+++ pf-libs/pf-core-api/src/main/kotlin/br/com/pratofeito/common/domain/api/model/valueObjects.kt
@@ -1,5 +1,14 @@
 package br.com.pratofeito.common.domain.api.model
 
+import java.math.BigDecimal
 import java.util.Date
 
 data class AuditEntry(val who: String, val `when`: Date)
+
+data class PersonName(val firstName: String, val lastName: String)
+
+data class Money(val amout: BigDecimal) {
+    fun add(delta: Money): Money = Money(amout.add(delta.amout))
+    fun multiply(multiplier: Int): Money = Money(amout.multiply(BigDecimal(multiplier)))
+    fun isGreatherThanOrEqual(moneyCompare: Money): Boolean = amout >= moneyCompare.amout
+}
--- pf-libs/pf-core-api/src/main/kotlin/br/com/pratofeito/customer/domain/api/commands.kt
+++ pf-libs/pf-core-api/src/main/kotlin/br/com/pratofeito/customer/domain/api/commands.kt
@@ -1,1 +1,35 @@
 package br.com.pratofeito.customer.domain.api
+
+import br.com.pratofeito.common.domain.api.AuditableAbstractCommand
+import br.com.pratofeito.common.domain.api.model.AuditEntry
+import br.com.pratofeito.common.domain.api.model.Money
+import br.com.pratofeito.common.domain.api.model.PersonName
+import br.com.pratofeito.customer.domain.api.model.CustomerId
+import br.com.pratofeito.customer.domain.api.model.CustomerOrderId
+import org.axonframework.modelling.command.TargetAggregateIdentifier
+import javax.validation.Valid
+
+abstract class CustomerCommand(open val targetAggregateIdentifier: CustomerId, override val auditEntry: AuditEntry)
+    : AuditableAbstractCommand(auditEntry)
+
+abstract class CustomerOrderCommand(open val targetAggregateIdentifier: CustomerOrderId, override val auditEntry: AuditEntry)
+    : AuditableAbstractCommand(auditEntry)
+
+abstract class CreateCustomerCommand(@TargetAggregateIdentifier override val targetAggregateIdentifier: CustomerId,
+                                     @field:Valid val name: PersonName,
+                                     val orderLimit: Money,
+                                     override val auditEntry: AuditEntry)
+    : CustomerCommand(targetAggregateIdentifier, auditEntry)
+
+abstract class CreateCustomerOrderCommand(@TargetAggregateIdentifier override val targetAggregateIdentifier: CustomerId,
+                                          val customerOrderId: CustomerOrderId,
+                                          @field:Valid val orderTotal: Money,
+                                          override val auditEntry: AuditEntry)
+    : CustomerCommand(targetAggregateIdentifier, auditEntry) {
+        constructor(targetAggregateIdentifier: CustomerId, orderTotal: Money, auditEntry: AuditEntry)
+                : this(targetAggregateIdentifier, CustomerOrderId(), orderTotal, auditEntry)
+    }
+
+abstract class MarkCustomerOrderAsDelivereCommand(@TargetAggregateIdentifier override val targetAggregateIdentifier: CustomerOrderId,
+                                                  override val auditEntry: AuditEntry)
+    : CustomerOrderCommand(targetAggregateIdentifier, auditEntry)
